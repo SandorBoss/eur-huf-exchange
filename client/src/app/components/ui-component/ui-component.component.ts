7dfe45e5a34cf343d059d626e1cc9df2ce039ee3
SandorBoss <boszormenyi.s@gmail.com>
2023-04-11 10:53:59 +0200

    generate ui component

---
+++ client/src/app/components/ui-component/ui-component.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-component',
+  templateUrl: './ui-component.component.html',
+  styleUrls: ['./ui-component.component.scss']
+})
+export class UiComponentComponent {
+
+  /* 
+   * currencies as input
+   *
+   * service calls
+   */
+
+}
